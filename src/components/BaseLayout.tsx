24dbb64ffd92d1156166fef99771f8d0f7ae6894
AJ <ajlai24@users.noreply.github.com>
2025-01-06 14:28:58 -0800

    Update README (#24)
    * Update footer and readme (#23)
    
    * Bump next from 15.0.4 to 15.1.3 (#22)
    
    Bumps [next](https://github.com/vercel/next.js) from 15.0.4 to 15.1.3.
    - [Release notes](https://github.com/vercel/next.js/releases)
    - [Changelog](https://github.com/vercel/next.js/blob/canary/release.js)
    - [Commits](https://github.com/vercel/next.js/compare/v15.0.4...v15.1.3)
    
    ---
    updated-dependencies:
    - dependency-name: next
      dependency-type: direct:production
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>

--- src/components/BaseLayout.tsx
+++ src/components/BaseLayout.tsx
@@ -60,6 +60,10 @@ export default async function BaseLayout({ children, locale }: Props) {
               <footer className="border-t border-border/40 py-6 dark:border-border md:px-8 md:py-0">
                 <div className="text-xs text-neutral-500 dark:text-neutral-400 flex flex-col items-center justify-between gap-4 md:h-24 md:flex-row">
                   &copy; {year} nehno.com
+                  <div className="text-center">
+                    Made with Next.js, Tailwind, shadcn/ui, next-intl,
+                    Sanity.io, and Supabase
+                  </div>
                   <div className="flex gap-2 align-middle">
                     <Button variant="ghost" size="icon">
                       <Link href="https://github.com/ajlai24" target="_blank">
