ecfbd91db3d3b37fe386a5a4376c4f97cb459ca4
ajlai <ajlai24@users.noreply.github.com>
2026-01-02 17:16:52 -0800

    Improve ai chat page

--- src/components/BaseLayout.tsx
+++ src/components/BaseLayout.tsx
@@ -1,15 +1,13 @@
 import Loading from "@/app/[locale]/loading";
 import { Navigation } from "@/components/Navigation";
 import { ThemeProvider } from "@/components/theme-provider";
-import { Button } from "@/components/ui/button";
-import { Icons } from "@/components/ui/icons";
 import { Analytics } from "@vercel/analytics/react";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 import { NextIntlClientProvider } from "next-intl";
 import { getMessages } from "next-intl/server";
 import { Geist } from "next/font/google";
-import Link from "next/link";
 import { ReactNode, Suspense } from "react";
+import { Footer } from "./Footer";
 
 const geist = Geist({
   subsets: ["latin"],
@@ -41,36 +39,14 @@ export default async function BaseLayout({ children, locale }: Props) {
               <div className="mx-auto w-full border-border/40 dark:border-border min-[1800px]:max-w-[1536px] min-[1800px]:border-x">
                 <Navigation />
                 <main className="flex-1 overflow-y-auto">
-                  <div className="container mx-auto min-h-[calc(100vh-3.5rem)] pt-8 p-4 flex flex-col">
+                  <div className="container mx-auto min-h-[calc(100vh-57px)] pt-8 p-4 flex flex-col">
                     <Suspense fallback={<Loading />}>{children}</Suspense>
                     <SpeedInsights />
                     <Analytics />
                   </div>
                 </main>
-                <footer className="border-t border-border/40 py-6 dark:border-border md:px-8 md:py-0">
-                  <div className="text-xs text-neutral-500 dark:text-neutral-400 flex flex-col items-center justify-between gap-4 md:h-24 md:flex-row">
-                    &copy; {year} nehno.com
-                    <div className="text-center">
-                      Made with Next.js, Tailwind, shadcn/ui, next-intl,
-                      Sanity.io, and Supabase
-                    </div>
-                    <div className="flex gap-2 align-middle">
-                      <Button variant="ghost" size="icon">
-                        <Link href="https://github.com/ajlai24" target="_blank">
-                          <Icons.gitHub className="h-6 w-6" />
-                        </Link>
-                      </Button>
-                      <Button variant="ghost" size="icon">
-                        <Link
-                          href="https://www.linkedin.com/in/devajlai/"
-                          target="_blank"
-                        >
-                          <Icons.linkedin className="h-6 w-6" />
-                        </Link>
-                      </Button>
-                    </div>
-                  </div>
-                </footer>
+
+                <Footer />
               </div>
             </div>
           </ThemeProvider>
