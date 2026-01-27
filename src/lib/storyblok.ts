354ef1130986dd2400f367f76d177a542ef6b814
ajlai <ajlai24@users.noreply.github.com>
2026-01-26 23:23:21 -0800

    Update storyblok config

--- src/lib/storyblok.ts
+++ src/lib/storyblok.ts
@@ -1,7 +1,7 @@
 import { apiPlugin, storyblokInit } from "@storyblok/react/rsc";
 
 export const getStoryblokApi = storyblokInit({
-  accessToken: process.env.NEXT_PUBLIC_STORYBLOK_CONTENT_API_ACCESS_TOKEN,
+  accessToken: process.env.STORYBLOK_API_TOKEN,
   use: [apiPlugin],
   apiOptions: {
     region: "eu",
