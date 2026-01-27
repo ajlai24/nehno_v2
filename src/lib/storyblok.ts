8e7d80e60b98a4ba6ca5fbf548c16830a2298c3c
ajlai <ajlai24@users.noreply.github.com>
2026-01-26 23:35:34 -0800

    Use NEXT_PUBLIC for storyblok token

--- src/lib/storyblok.ts
+++ src/lib/storyblok.ts
@@ -1,7 +1,7 @@
 import { apiPlugin, storyblokInit } from "@storyblok/react/rsc";
 
 export const getStoryblokApi = storyblokInit({
-  accessToken: process.env.STORYBLOK_API_TOKEN,
+  accessToken: process.env.NEXT_PUBLIC_STORYBLOK_API_TOKEN,
   use: [apiPlugin],
   apiOptions: {
     region: "eu",
