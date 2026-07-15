bea236d390bac0b0cc757eb1e0b43ffeca84bd4c
AJ <ajlai24@users.noreply.github.com>
2026-07-15 15:38:07 -0700

    Fix switches search (#113)
    * Fix switches search
    
    * Fix switches search

--- src/app/[locale]/switches/queries.ts
+++ src/app/[locale]/switches/queries.ts
@@ -1,26 +1,15 @@
-import { createClient } from "@supabase/supabase-js";
+import { Tables } from "@/utils/supabase/supabase.types";
 
-const SUPABASE_URL = process.env.NEXT_PUBLIC_SUPABASE_URL!;
-const SUPABASE_ANON_KEY = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
-const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+export const getSearchSuggestions = async (
+  searchInput: string,
+): Promise<Tables<"switches">[]> => {
+  const response = await fetch(
+    `/api/switches/search?query=${encodeURIComponent(searchInput)}`,
+  );
 
-export const getFilters = async () => {
-  return await supabase.rpc("get_filters");
-};
-
-export const getSearchSuggestions = async (searchInput: string) => {
-  let query = supabase.from("switches").select();
-  query = query
-    .or(
-      `name.ilike.%${searchInput}%,brand.ilike.%${searchInput}%,series.ilike.%${searchInput}%`,
-    )
-    .limit(5);
-
-  const { data: searchSuggestions, error } = await query;
-
-  if (error) {
+  if (!response.ok) {
     throw new Error("Error fetching search suggestions");
   }
 
-  return searchSuggestions;
+  return response.json();
 };
